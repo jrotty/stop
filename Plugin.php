e1b30bacec857030aab72aa1f2513d6c00729468
jrotty <bssf@qq.com>
2020-08-05 10:14:30 +0800

    1.5.0版本

---
+++ Plugin.php
@@ -0,0 +1,75 @@
+<?php
+if (!defined('__TYPECHO_ROOT_DIR__')) exit;
+
+/**
+ * 游客止步，后台所有界面不允许进入，需要管理员在前台登录后才允许进入后台页面
+ * 
+ * @package 游客止步
+ * @author 张老师
+ * @version 1.5
+ * @link https://qqdie.com
+ */
+class stop_Plugin implements Typecho_Plugin_Interface
+{
+
+    /**
+     * 激活插件方法,如果激活失败,直接抛出异常
+     * 
+     * @access public
+     * @return void
+     * @throws Typecho_Plugin_Exception
+     */
+    public static function activate()
+    {
+        Typecho_Plugin::factory('admin/common.php')->begin = array('stop_Plugin', 's');
+        Typecho_Plugin::factory('Widget_User')->logout = array('stop_Plugin', 'm');
+    }
+
+    /**
+     * 禁用插件方法,如果禁用失败,直接抛出异常
+     * 
+     * @statics
+     * @access public
+     * @return void
+     * @throws Typecho_Plugin_Exception
+     */
+    public static function deactivate(){}
+
+    /**
+     * 获取插件配置面板
+     * 
+     * @access public
+     * @param Typecho_Widget_Helper_Form $form 配置面板
+     * @return void
+     */
+    public static function config(Typecho_Widget_Helper_Form $form)
+    {
+    }
+
+    /**
+     * 个人用户的配置面板
+     * 
+     * @access public
+     * @param Typecho_Widget_Helper_Form $form
+     * @return void
+     */
+    public static function personalConfig(Typecho_Widget_Helper_Form $form)
+    {}
+
+    public static function s()
+    {
+Typecho_Widget::widget('Widget_User')->to($user);
+Typecho_Widget::widget('Widget_Options')->to($options);
+if(!$user->pass('editor', true)){
+header("HTTP/1.0 404 Not Found");
+header("Status: 404 Not Found");
+exit;
+}
+    }
+public static function m(){
+$url = Helper::options()->siteUrl;
+Header("Location:$url"); 
+@session_destroy();
+ exit;
+}
+}
