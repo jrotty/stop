9803fa1d69109e7cc7eb89bc9ac1dd1c1386db1e
jrotty <bssf@qq.com>
2020-11-11 16:44:45 +0800

    Update Plugin.php

--- Plugin.php
+++ Plugin.php
@@ -2,11 +2,11 @@
 if (!defined('__TYPECHO_ROOT_DIR__')) exit;
 
 /**
- * 游客止步，后台所有界面不允许进入，需要管理员在前台登录后才允许进入后台页面
+ * 游客止步，后台所有界面不允许进入，需要管理员在前台登录后才允许进入后台页面；同时附件页面不允许非管理员用户访问！
  * 
  * @package 游客止步
  * @author 张老师
- * @version 1.5
+ * @version 1.6
  * @link https://qqdie.com/archives/stop-plugin-typecho.html
  */
 class stop_Plugin implements Typecho_Plugin_Interface
@@ -23,6 +23,7 @@ class stop_Plugin implements Typecho_Plugin_Interface
     {
         Typecho_Plugin::factory('admin/common.php')->begin = array('stop_Plugin', 's');
         Typecho_Plugin::factory('Widget_Logout')->logout = array('stop_Plugin', 'm');
+        Typecho_Plugin::factory('Widget_Archive')->header = array('stop_Plugin', 'header');
     }
 
     /**
@@ -72,4 +73,22 @@ Header("Location:$url");
 @session_destroy();
  exit;
 }
+
+public static function header($h, $obj){
+$txt=$h;
+if($obj->is('attachment')){
+Typecho_Widget::widget('Widget_User')->to($user);
+Typecho_Widget::widget('Widget_Options')->to($options);
+if(!$user->pass('editor', true)){
+$url = Helper::options()->siteUrl;
+Header("Location:$url"); 
+@session_destroy();
+ exit;
+}
+}
+return $txt;
+}
+
+
+
 }
